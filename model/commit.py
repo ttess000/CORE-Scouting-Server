40e560e032f37afceaf5dd8b1e6fa93ce32792a5
ttess000 <ttessman000@stu.waukesha.k12.wi.us>
2015-01-17 13:18:40 -0600

    Update Field Names for 2015

--- model/commit.py
+++ model/commit.py
@@ -106,9 +106,9 @@ class MatchCommit(me.Document):
     ## Autonomous
     ###########
 
-    auto_high = IntField(
-        verbose_name = "High Goal",
-        help_text= "How many balls did the team scouted shoot in the high goal in autonomous?",
+    tote_bonus = IntField(
+        verbose_name = "Tote Bonus",
+        help_text= "How many totes did the team push into the scoring zone?",
         max_value = 3,
         min_value = 0,
         default = 0
@@ -116,9 +116,10 @@ class MatchCommit(me.Document):
     @property
     def auto_high_s(self):
         return self.auto_high != 0
-    auto_low = IntField(
-        verbose_name = "Low Goal",
-        help_text = "How many balls did the team scouted score in the low goal in autonomous?",
+        
+    bin_bonus = IntField(
+        verbose_name = "Bin Bonus",
+        help_text = "How many recycle bins did the team push into the scoring zone?",
         max_value = 3,
         min_value = 0,
         default = 0
@@ -126,200 +127,100 @@ class MatchCommit(me.Document):
     @property
     def auto_low_s(self):
         return self.auto_low != 0
-    auto_hot = IntField(
-        verbose_name = "Hot Goal",
-        help_text = "How many hot scores did the team get in auto?",
-        max_value = 3,
-        min_value = 0,
-        default = 0
+    move_bonus = BooleanField(
+        verbose_name = "Robot Bonus",
+        help_text = "Did the team move into the scoring zone?",
+        default = False
     )
     @property
     def auto_hot_s(self):
         return self.auto_hot != 0
-    @property
-    def auto_balls(self):
-        return self.auto_low+self.auto_high
-    auto_mobility = BooleanField(
-        verbose_name = "Mobility",
-        help_text = "Did the team receive mobility points in autonomous?",
-        default = False
-    )
-    auto_goalie = BooleanField(
-        verbose_name = "Goalie Zone",
-        help_text = "Did the team scouted start the match in the goalie zone?",
-        default = False
-    )
-    # auto_balls = IntField(
-    #     verbose_name = "Balls Scored",
-    #     help_text= "The number of balls scored by the team scouted in Autonomous",
-    #     max_value = 3,
-    #     min_value = 0,
-    #     default= 0
-    # )
 
     @property
     def auto_contrib(self):
         return sum((
-            self.auto_hot*15,
-            self.auto_high*10,
-            self.auto_low*5,
-            self.auto_mobility * 5
+            self.tote_bonus,
         ))
 
     ###########
     ## Teleop
     ###########
-
-    team_cycles = LessThanField(
-        verbose_name =  "Team Cycles",
-        help_text =  "The number of times the team contributes assist points.",
-        min_value = 0,
-        less_than = "alliance_cycles",
-        default = 0
-    )
-    alliance_cycles = IntField(
-        verbose_name =  "Alliance Cycles",
-        help_text =  "The number of times that the entire alliance completes a full cycle.",
+    
+    grey_totes_scored = IntField(
+        verbose_name =  "Grey Totes",
+        help_text =  "The number of grey totes placed into scoring position",
         min_value = 0,
         default = 0
     )
-    truss_total = IntField(
-        verbose_name =  "Truss Shot Total",
-        help_text =  "The number of times the team scouted attempted to score a truss shot",
+    highest_grey_tote = IntField(
+        verbose_name =  "Highest Tote",
+        help_text =  "What was the highest tote scored?",
         min_value = 0,
         default = 0
     )
-    truss_made = LessThanField(
-        verbose_name =  "Truss Shots Made",
-        help_text =  "The number of times the team scouted successfully scored a truss shot",
-        min_value = 0,
-        less_than = "truss_total",
-        default = 0
-    )
-    catch_total = IntField(
-        verbose_name =  "Catch Total",
-        help_text =  "The number of times the team scouted attempted to catch a ball over the truss",
+    bins_scored = IntField(
+        verbose_name =  "Recycle Bin",
+        help_text =  "The number of Recycle Bins Scored",
         min_value = 0,
         default = 0
     )
-    catch_made = LessThanField(
-        verbose_name =  "Catches Made",
-        help_text =  "The number of times the team scouted successfully caught a ball over the truss",
+    highest_bin = IntField(
+        verbose_name =  "Highest Bin",
+        help_text =  "What was the highest bin scored?",
         min_value = 0,
         less_than = "catch_total",
         default = 0
     )
-    high_total = IntField(
-        verbose_name =  "High Goals Total",
-        help_text =  "The number of times the team scouted attempted to score in the high goal",
+
+    litter_landfill = IntField(
+        verbose_name =  "Landfill Litter",
+        help_text =  "The amount of litter scored in the Landfill",
+        min_value = 0,
+        less_than = "11",
+        default = 0
+    )
+    litter_thrown = IntField(
+        verbose_name =  "Outsourced Litter",
+        help_text =  "The number of times the team scouted attempted put litter in the other teams field",
         min_value = 0,
         default = 0
     )
-    high_made = LessThanField(
-        verbose_name =  "High Goals Made",
-        help_text =  "The number of times the team scouted successfully scored in the high goal",
-        min_value = 0,
-        less_than = "high_total",
-        default = 0
-    )
-    low_total = IntField(
-        verbose_name =  "Low Goal Total",
-        help_text =  "The number of times the team scouted attempted to score in the low goal",
+    yellow_totes_scored = IntField(
+        verbose_name =  "Yellow Totes",
+        help_text =  "The number of times the team scouted placed a yellow tote on the COOP bump.",
         min_value = 0,
         default = 0
     )
-    low_made = LessThanField(
-        verbose_name =  "Low Goals Made",
-        help_text =  "The number of times the tea, scouted successfully scored in the low goal",
-        min_value = 0,
-        less_than = "low_total",
-        default = 0
-    )
-    pickup_total = IntField(
-        verbose_name =  "Pickup Total",
-        help_text =  "the number of times the team scouted attempted to pick up a ball",
-        min_value = 0,
-        default = 0
-    )
-    pickup_made = LessThanField(
-        verbose_name =  "Pickups Made",
-        help_text =  "The number of times the team scouted successfully picked up a ball",
-        min_value = 0,
-        less_than = "pickup_total",
-        default = 0
-    )
-    pass_total = IntField(
-        verbose_name =  "Pass Total",
-        help_text =  "The number of times the team scouted attempted to pass the ball ",
-        min_value = 0,
-        default = 0
-    )
-    pass_made = LessThanField(
-        verbose_name =  "Passes Made",
-        help_text =  "The number of times the team scouted successfully passed the ball",
-        min_value = 0,
-        less_than = "pass_total",
-        default = 0
-    )
-    inbound_total = IntField(
-        verbose_name =  "Inbound Total",
-        help_text =  "The number of times the team attempted to inbound a ball ",
-        min_value = 0,
-        default = 0
-    )
-    inbound_made = LessThanField(
-        verbose_name =  "Inbounds Made",
-        help_text =  "The number of times the team successfully inbounded a ball",
-        min_value = 0,
-        less_than = "inbound_total",
-        default = 0
-    )
-    defense = BooleanField(
-        verbose_name =  "Defense",
-        help_text =  "Did the team scouted actively play defense on other robots?",
-        default = False
-    )
+    
 
     @property
     def tele_contrib(self):
         return sum((
-            self.team_cycles*5,
-            self.truss_made*10,
-            self.catch_made*10,
-            self.low_made*1,
-            self.high_made*10
+            self.grey_totes_scored,
         ))
 
 
     #############
     ## Robot Info
     #############
-    r_infos = ["shooter", "catcher", "pickup", "blocker", "goalie"]
-    shooter = BooleanField(
+    r_infos = ["tote_pickup", "bin_pickup", "litter_pickup"]
+    tote_pickup = BooleanField(
         verbose_name =  "Shooter",
         help_text =  "Did the robot being scouted have a shooter mechanism?",
         default = False
     )
-    catcher = BooleanField(
+    bin_pickup = BooleanField(
         verbose_name =  "Catcher",
         help_text =  "Did the robot being scouted have a catching mechanism",
         default = False
     )
-    pickup = BooleanField(
+    litter_pickup = BooleanField(
         verbose_name =  "Pickup",
         help_text =  "Did the robot being scouted have a pickup mechanism?",
         default = False
     )
-    blocker = BooleanField(
-        verbose_name =  "Blocker",
-        help_text =  "Did the robot being scouted have a blocking mechanism?",
-        default = False
-    )
-    goalie = BooleanField(
-        verbose_name =  "Goalie Mechanism",
-        help_text =  "Did the robot being scouted have a goalie mechanism?",
-        default = False
-    )
+
+   
     drive_type = StringField(
         verbose_name =  "Drive Type",
         choices = [("tank", "Tank"), ("strafe", "Strafe")],
@@ -328,16 +229,12 @@ class MatchCommit(me.Document):
         default = ""
     )
 
-    hp_front = StringField(
-        verbose_name =  "Human Player Front",
+    hp = StringField(
+        verbose_name =  "Human Player",
         choices = (("",""),("+","+"),("-","-")),
         default = ""
     )
-    hp_back = StringField(
-        verbose_name =  "Human Player Back",
-        choices = (("",""),("+","+"),("-","-")),
-        default = ""
-    )
+
 
     @property
     def match(self):
@@ -349,13 +246,11 @@ class MatchCommit(me.Document):
         return self.auto_contrib + self.tele_contrib
     @property
     def n_hp_f(self):
-        return 1 if self.hp_front == "+" else -1 if self.hp_front == "-" else 0
-    @property
-    def n_hp_b(self):
-        return 1 if self.hp_back == "+" else -1 if self.hp_back == "-" else 0
+        return 1 if self.hp == "+" else -1 if self.hp_front == "-" else 0
+  
     @property
     def n_hp(self):
-        return self.n_hp_f + self.n_hp_b
+        return self.n_hp_f
 
 def parse_cid(cid):
     cid = cid.split("_")
